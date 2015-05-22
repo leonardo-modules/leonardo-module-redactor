4224da454d35d28bbee40557e630de66f4e6404f
Michael Kutý <6du1ro.n@gmail.com>
2015-05-22 23:22:28 +0200

    finalize

--- leonardo_module_redactor/__init__.py
+++ leonardo_module_redactor/__init__.py
@@ -1,4 +1,6 @@
 
+from django.utils.translation import ugettext_lazy as _
+
 from django.apps import AppConfig
 
 default_app_config = 'leonardo_module_redactor.RedactorConfig'
@@ -6,12 +8,15 @@ default_app_config = 'leonardo_module_redactor.RedactorConfig'
 
 class Default(object):
 
+    urls_conf = 'redactor.urls'
+
     apps = [
         'redactor',
         'leonardo_module_redactor',
     ]
 
     js_files = [
+        'redactor/redactor.init.js',
         'redactor/redactor.js',
         'redactor/plugins/table.js',
         'redactor/plugins/video.js',
@@ -25,9 +30,20 @@ class Default(object):
         'redactor/css/redactor.css'
     ]
 
+    config = {
+        'REDACTOR_UPLOAD': ('uploads/', _('Redactor upload directory')),
+    }
+
 
 class RedactorConfig(AppConfig, Default):
     name = 'leonardo_module_redactor'
-    verbose_name = ("Leonardo Redactor")
+    verbose_name = _("Leonardo Redactor")
+
+    def ready(self):
+
+        # path forms
+        from leonardo.module.web.widgets import forms as widget_forms
+        from redactor.widgets import RedactorEditor
+        widget_forms.WIDGETS['text'] = RedactorEditor()
 
 default = Default()
